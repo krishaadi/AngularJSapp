0617dfc7f4e5866f07c064db6c20a15cd1451408
Adkrishna <encaditya@gmail.com>
2020-04-29 04:12:01 +0530

    serverchange

--- server.js
+++ server.js
@@ -1,44 +1,14 @@
-const http = require('http');
-const url = require('url');
-const path =require('path');
-const fs = require('fs');
-const mimeTypes = {
-	"html": "text/html" ,
-	"jpeg": "image/jpeg",
-	"jpg": "image/jpg",
-	"png":"image/png",
-	"js" :"text/javascript",
-	"css" :"text/css"
-};
+const express = require('express');
+const path = require('path');
+const app = express();
 
-http.createServer(function(req,res){
-	var uri = url.parse(req.url).pathname;
-	var fileName = path.join(process.cwd(), unescape(uri));
-	console.log('Loading ' + uri);
-	var stats;
-	//create try catch block
-	try {
-		stats = fs.lstatSync(fileName);
-	} catch (e) {
-		res.writeHead(404, {'Content-type': 'text/plai'});
-		res.write('404 Not Found\n');
-		res.end();
-		return;
-	}
+// Serve static files....
+app.use(express.static(__dirname + '/'));
 
-	if(stats.isFile()){
-		var mimeType = mimeTypes[path.extname(fileName).split]
-		res.writeHead(200, {'Content-type': mimeTypes});
-		var fileStream = fs.createReadStream(fileName);
-		fileStream.pipe(res);		
-	}else if(stats.isDirectory()){
-		res.writeHead(200, {
-			'Location': 'index.html'
-		});
-		res.end();
-	}else{
-		res.writeHead(500, {'Content-type': 'text/plain'});
-		res.write('500 Internal Error\n');
-		res.end();
-	}
-}).listen(3000);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
