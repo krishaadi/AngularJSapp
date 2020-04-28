b254ed7e60d245a015ed69fc85d88ec7c6803758
Adkrishna <encaditya@gmail.com>
2020-04-29 03:40:16 +0530

    serverchange

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
+router.get('/home',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
